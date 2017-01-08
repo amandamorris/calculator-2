dfc13b010020ca247e03fe390f39e7bef00b3f10
Amanda Morris <amandafmorris@gmail.com>
2017-01-08 20:31:35 +0000

    Used list slicing to fix hack in subtract and divide

--- arithmetic.py
+++ arithmetic.py
@@ -6,12 +6,14 @@ def add(number_list):
         summation = summation + int(num)
     return summation
 
+
 def subtract(number_list):
     """Return the difference of numbers"""
-    difference = int(number_list[0]) * 2
-    for num in number_list:
+    difference = int(number_list[0])
+    for num in number_list[1:]:
         difference = difference - int(num)
     return difference
+
 
 def multiply(number_list):
     """Return the product of numbers"""
@@ -20,6 +22,7 @@ def multiply(number_list):
         product = product * int(num)
     return product
 
+
 def divide(number_list):
     """Return the quotient of numbers as a float"""
     dividor = float(number_list[0]) ** 2
@@ -27,11 +30,13 @@ def divide(number_list):
         dividor = dividor / float(num)
     return dividor
 
+
 def square(number_list):
     """Return the square of a list of numbers"""
     for num in number_list:
         print int(num) ** 2
     return ""
+
 
 def cube(number_list):
     """Return the cube of a list of numbers"""
@@ -39,9 +44,11 @@ def cube(number_list):
         print int(num) ** 3
     return ""
 
+
 def power(num, exponent):
     """Return num raised to the power of exponent"""
     return num ** exponent
+
 
 def mod(num1, num2):
     """Return remainder of num1 divided by num2"""
